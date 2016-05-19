58b5577ff759be1597db4fcb499e727651ba00cc
Muqsit Rayyan <hackhack05@gmail.com>
2016-05-19 14:10:16 +0300

    Update Axe.php

--- src/CosmicAxe/Axe.php
+++ src/CosmicAxe/Axe.php
@@ -1,1 +1,37 @@
+<?php
 
+namespace CosmicAxe;
+
+use pocketmine\Player;
+use pocketmine\event\Listener;
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+use pocketmine\level\Level;
+use pocketmine\item\Item;
+use pocketmine\level\particle\HugeExplodeParticle;
+
+class Axe extends PluginBase implements Listener{
+	public function onEnable(){
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	public function onHurt(EntityDamageEvent $event){
+		if($event instanceof EntityDamageByEntityEvent){
+			$damager = $event->getDamager();
+			$level = $damager->getLevel();
+			if($damager instanceof Player){
+				if($damager->getInventory()->getItemInHand()->getId() === 279){
+				  switch(mt_rand(1, 3) == 1){
+              case 1:
+                $level->addParticle(new HugeExplodeParticle($damager->getLocation()));
+                break;
+              case 2:
+                break;
+              case 3:
+                break;
+        }
+			}
+		}
+	}
+}
